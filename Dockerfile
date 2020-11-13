9ef3149aa5cbb6db1c91f4b4574cb96b5abe4ff5
cameron <cameron.debliquy@epitech.eu>
2020-11-13 13:54:37 +0000

    add first commit

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,17 @@
 FROM node:12
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+#copie tout ce qui est dans le workdir dans le dossier courant.
+COPY . .
+
+RUN npm test
+
+EXPOSE 80
+
+CMD [ "node", "server.js" ]
+#ENTRYPOINT ["/", ""]
--- tests/integration/api.test.js
+++ tests/integration/api.test.js
@@ -1,10 +1,13 @@
 const request = require('supertest')
 const app = require('../../server')
-describe('Hello manager', () => {
-  it('should answer to the manager', async () => {
-    const res = await request(app)
-      .get('/')
-      .set({ 'role': ''})
-    expect(res.statusCode).toEqual()
+describe('Hello manager', () => 
+{
+  it('should answer to the manager', async () => 
+  {
+    await request(app)
+      .get('/hello')
+      .set({ 'role': 'employee'})
+      .expect(302)
+      .expect('Location', '/employee')
   })
 })
