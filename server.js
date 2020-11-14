d95a4002d42b77e6948306bba8059918515ccfe6
Ackermann <theoackrm@gmail.com>
2020-11-14 08:49:07 +0000

    up ex6.6

--- server.js
+++ server.js
@@ -13,8 +13,24 @@ app.get('/hello', (req, res) => {
     res.redirect('/' + req.headers.role).end()
 });
 app.get('/manager', (req, res) => {
+  var mysql      = require('mysql');
+  var connection = mysql.createConnection({
+  host     : 'mysql',
+  user     : 'root',
+  password : '',
+  database : 'db_test'
+});
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
   res.send('hello manager')
 });
+
 app.get('/employee', (req, res) => {
   res.send('hello employee') 
 });
