daa59b97b7ace0f2a310c68d4ad2b2931dc289a9
Gabriel Riboldi <gabriel.riboldi@epitech.eu>
2020-11-14 10:13:10 +0000

    Remove mysql usage

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+//var mysql  = require('mysql');
 
 // App
 const app = express();
@@ -13,8 +14,25 @@ app.get('/hello', (req, res) => {
     res.redirect('/' + req.headers.role).end()
 });
 app.get('/manager', (req, res) => {
+  //console.log("Environment variables:", process.env)
+/*  var connection = mysql.createConnection({
+    host     : process.env.MYSQL_HOST,
+    user     : process.env.MYSQL_USER,
+    password : process.env.MYSQL_PASSWORD,
+    database : process.env.MYSQL_DATABASE
+  });
+   
+  connection.connect();
+  
+  connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+  console.log("Passage avant res.send <<<<<<<<<<<<<")*/
   res.send('hello manager')
+  //connection.end();
 });
+
 app.get('/employee', (req, res) => {
   res.send('hello employee') 
 });
